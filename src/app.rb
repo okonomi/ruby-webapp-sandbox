a456b82b3b845eb5b9c3f52dc453e9fa25c0dbe8
okonomi <oknm@okonomi.sakura.ne.jp>
2018-09-08 23:33:12 +0900

    Render haml template

--- src/app.rb
+++ src/app.rb
@@ -1,9 +1,13 @@
+require 'haml'
+
 class App
   def call(env)
     [
       200,
-      { 'Content-Type' => 'text/plain' },
-      ['hello, rack']
+      { 'Content-Type' => 'text/html' },
+      [
+        Haml::Engine.new(File.read('src/view/app.html.haml')).render
+      ]
     ]
   end
 end
